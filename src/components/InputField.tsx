4cc82c8c2343dc182d42c252aff59bcf44d986c4
baroiroi <johnroy0414@gmail.com>
2024-02-23 10:14:59 +0800

    installed react-beautiful dnd

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -12,8 +12,7 @@ const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
   return (
     <form className='input' onSubmit={(e) => {
       handleAdd(e);
-      inputRef.current?.blur();
-      inputRef.current?.focus()}}>
+      inputRef.current?.blur();}}>
       <input 
         ref={inputRef} 
         type='input'  
